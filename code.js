c1f9c5d07fe30cb1164e0a19c35e863c3bdbcaa8
Your Name Here <emercado@LibiMac02-16.local>
2014-05-14 12:27:57 -0700

    added

--- code.js
+++ code.js
@@ -1,14 +1,3 @@
-<<<<<<< HEAD
-
-<body>
-                <p id="coo">Guess what </p>
-                <input type="button" onclick="changeText()" value="click me" />
-                <p id="no"> Whatever you do, do not click the button </p>
-                <input type="button" onclick="change()" value="click me" >
-
-               <script type="text/javascript">
-=======
->>>>>>> 3792e591586ffea1ebadef8aca3c8ba64137cc07
                         function changeText() {
                                 // alert("hi");
                                 $("#coo").html("eddy was here");
@@ -23,8 +12,3 @@
                         }
 
 
-<<<<<<< HEAD
-                </script>
-=======
-                
->>>>>>> 3792e591586ffea1ebadef8aca3c8ba64137cc07
